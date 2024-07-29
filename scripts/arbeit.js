5d5d0c53be78bdf7152b9fdb61081ddece5db6d4
rudipsq <132471589+rudipsq@users.noreply.github.com>
2024-07-29 22:56:08 +0200

    update

--- scripts/arbeit.js
+++ scripts/arbeit.js
@@ -13,15 +13,58 @@ async function fillBearbeiteteUrkundenListe() {
     return;
   }
 
-  const container = document.getElementById("arbeitListe");
   data.slice(3).forEach((item) => {
     if (item[0] != "") {
+      // tabelle reihe bauen
       const row = document.createElement("div");
       row.innerHTML = `
       <p>${item[0]}</p>
       <p>${item[3]}</p>
     `;
+
+      let container;
+
+      // element in kategorien sortieren
+      switch (item[2]) {
+        case "Könige und Kaiser":
+          container = document.getElementById("arbeitListe1");
+          break;
+
+        case "Papsturkunde":
+          container = document.getElementById("arbeitListe2");
+          break;
+
+        case "Familiengeschichten?":
+          container = document.getElementById("arbeitListe3");
+          break;
+
+        case "test4":
+          container = document.getElementById("arbeitListe4");
+          break;
+
+        case "test5":
+          container = document.getElementById("arbeitListe5");
+          break;
+
+        default:
+          break;
+      }
+
+      console.log(container);
+
       container.appendChild(row);
     }
   });
+}
+
+function showArbeitListe(kategorie) {
+  if (kategorie == null) return;
+
+  for (let i = 1; i <= 5; i++) {
+    document.getElementById("arbeitListe" + i).style.display = "none";
+    document.getElementById("arbeitButton" + i).classList.remove("active");
+  }
+
+  document.getElementById("arbeitListe" + kategorie).style.display = "block";
+  document.getElementById("arbeitButton" + kategorie).classList.add("active");
 }
